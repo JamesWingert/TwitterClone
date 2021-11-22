0402a589bbc1c00e0693fa85109584febf94861f
JamesWingert <jameswingert1@gmail.com>
2021-11-22 16:57:01 -0500

    Added Tweets to the model, created tweets decorator

---
+++ twitter/src/api/tweets.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify, abort, request
+from ..models import Tweet, User, db
+
+bp = Blueprint('tweets', __name__, url_prefix='/tweets')
+
+@bp.route('', methods=['GET']) # decorator takes path and list of HTTP verbs
+def index():
+    tweets = Tweet.query.all() # ORM performs SELECT query
+    result = []
+    for t in tweets:
+        result.append(t.serialize()) # build list of Tweets as dictionaries
+    return jsonify(result) # return JSON response
